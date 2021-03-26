3dacb0443652a5720301d3077b82712bcc799568
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-03-26 17:38:12 +0300

    oop2 final

---
+++ OOP2/CorporateCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class CorporateCustomer : Customer
+    {
+        public string CompanyName { get; set; }
+        public string TaxNo { get; set; }
+    }
+}
--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -6,7 +6,25 @@ namespace OOP2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IndividualCustomer cust1 = new IndividualCustomer();
+            cust1.CustomerNo = "12345";
+            cust1.Name = "Berkc";
+            cust1.Surname = "Tezc";
+            cust1.Surname = "1111";
+            cust1.Id = 1;
+
+            CorporateCustomer cust2 = new CorporateCustomer();
+            cust2.CompanyName = "BT";
+            cust2.CustomerNo = "1221";
+            cust2.Id = 2;
+            cust2.TaxNo = "2142";
+
+            Customer cust4 = new IndividualCustomer();
+            Customer cust5 = new CorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(cust1);
+            customerManager.Add(cust2);
         }
     }
 }
