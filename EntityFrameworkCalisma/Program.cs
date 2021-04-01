3959d6ba7ce65f0ec0abbce0f4d2b4edc582804c
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-01 17:06:57 +0300

    added ef to project

---
+++ EntityFrameworkCalisma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EntityFrameworkCalisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EF - ORM: Object Relational Mapping
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; } //PK
+        public int CategoryId { get; set; } //FK
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; } //smalint
+    }
+
+    public class NorthwindContext : DbContext
+    {
+
+    }
+}
