80ac8b628fcb1395185fde6d4d9bbf0a3ed30dfb
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-01 17:07:32 +0300

    refactored classes to seperate files

---
+++ EntityFrameworkCalisma/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityFrameworkCalisma
+{
+    public class Product
+    {
+        public int ProductId { get; set; } //PK
+        public int CategoryId { get; set; } //FK
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; } //smalint
+    }
+}
--- EntityFrameworkCalisma/Program.cs
+++ EntityFrameworkCalisma/Program.cs
@@ -11,19 +11,4 @@ namespace EntityFrameworkCalisma
             Console.WriteLine("Hello World!");
         }
     }
-
-    public class Product
-    {
-        public int ProductId { get; set; } //PK
-        public int CategoryId { get; set; } //FK
-        public string ProductName { get; set; }
-        public string QuantityPerUnit { get; set; }
-        public decimal UnitPrice { get; set; }
-        public short UnitsInStock { get; set; } //smalint
-    }
-
-    public class NorthwindContext : DbContext
-    {
-
-    }
 }
